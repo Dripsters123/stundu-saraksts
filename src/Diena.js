18152ed356e87f6300a33def7b569a18539cc047
Your Name <you@example.com>
2023-10-04 11:36:45 +0300

    yayyyy

--- src/Diena.js
+++ src/Diena.js
@@ -1,14 +1,14 @@
 import Stunda from "./Stunda";
+
 function Diena(props){
- 
+ const stundasJSX = props.stundas.map((stunda) => {
+return <Stunda nosaukums= {stunda} />
+ });
     return (
         <>
         <h2>{props.diena}</h2>
         <ol>
-    <Stunda nosaukums= {props.stundas[0]} />
-    <Stunda nosaukums={props.stundas[1]}/>
-    <Stunda nosaukums= {props.stundas[2]}/>
-    <Stunda nosaukums={props.stundas[3]}/>
+  {stundasJSX}
     </ol>
    
 
