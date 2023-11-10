fdccb245e14b25b0713c6a41e5ec0404409fe9be
degviela <144688261+degviela@users.noreply.github.com>
2023-11-10 11:41:06 +0200

    useEffect

--- src/App.js
+++ src/App.js
@@ -1,58 +1,97 @@
 import Diena from "./Diena";
+import { useState, useEffect } from "react";
+
+
 
 function App() {
-    const pirmdienasStundas = [
-        "Sports pie Klintas",
-        "Dabaszinības",
-        "Vēsture",
-        "Krievu valoda"
-    ];
-    const otrdienasStundas = [
-        "Cita st",
-        "Vēl stunda",
-        "Iedod bucu comam",
-        "Kissing the homies"
-    ];
-    const visasStundas = [
-       {diena: "Pirmdiena",stundas:[
-        "Sports pie Klintas",
-        "Dabaszinības",
-        "Vēsture",
-        "Krievu valoda"  
-       ]},
-       {diena: "Otrdiena",stundas:[
-        "Cita st",
-        "Vēl stunda",
-        "Iedod bucu comam",
-        "Kissing the homies"
-       ]},
-       {diena:"Tresdiena",stundas:[
-        "Latviešu valoda un literatūra",
-       "Dabaszinības",
-        "Sociālās zinības un vēsture",
-     "Sistēmu programmēšana"
-       ]},
-       {diena:"Ceturtdiena",stundas:[
-        "Latval",
-        "Prog",
-        "Sport",
-        "Mat" 
-       ]},
-       {diena:"Piektdiena",stundas:[
-        "Prog",
-        "Dab",
-        "Fiz",
-        "Pats" 
-       ] }
-        ];
-    const dienasJSX = visasStundas.map((diena, indekss)=>{
-        return <Diena key={indekss} diena={diena.diena} stundas={diena.stundas}/>
-    });
-return ( <>
+    const [stundas, setStundas] = useState([]);
+    const [loading, setLoading] = useState(true);
 
-{dienasJSX}
- 
-</> )
+    useEffect(() => {
+        async function getLessons() {
+            const response = await fetch('https://cheese-cake.onthewifi.com/api/lessons');
+            const data = await response.json();
+            setStundas(data.IPb22);
+            const cleansedData = [
+                {
+                    diena: "Pirmdiena",
+                    stundas: data.IPb22[0].classes
+                },
+                {
+                    diena: "Otrdiena",
+                    stundas: data.IPb22[1].classes
+                },
+                {
+                    diena: "Tresdiena",
+                    stundas: data.IPb22[2].classes
+                },
+                {
+                    diena: "Ceturtdiena",
+                    stundas: data.IPb22[3].classes
+                },
+                {
+                    diena: "Piektdiena",
+                    stundas: data.IPb22[4].classes
+                },
+            ];
+            setStundas(cleansedData);
+            setLoading(false);
+        };
+        getLessons();
+
+    }, []);
+
+    const visasStundas = [
+        {
+            diena: "Pirmdiena", stundas: [
+                "Sports pie Klintas",
+                "Dabaszinības",
+                "Vēsture",
+                "Krievu valoda"
+            ]
+        },
+        {
+            diena: "Otrdiena", stundas: [
+                "Cita st",
+                "Vēl stunda",
+                "Iedod bucu comam",
+                "Kissing the homies"
+            ]
+        },
+        {
+            diena: "Tresdiena", stundas: [
+                "Latviešu valoda un literatūra",
+                "Dabaszinības",
+                "Sociālās zinības un vēsture",
+                "Sistēmu programmēšana"
+            ]
+        },
+        {
+            diena: "Ceturtdiena", stundas: [
+                "Latval",
+                "Prog",
+                "Sport",
+                "Mat"
+            ]
+        },
+        {
+            diena: "Piektdiena", stundas: [
+                "Prog",
+                "Dab",
+                "Fiz",
+                "Pats"
+            ]
+        }
+    ];
+
+    const dienasJSX = stundas.map((diena, indekss) => {
+        return <Diena key={indekss} diena={diena.diena} stundas={diena.stundas} />
+    });
+    return (<>
+
+        {loading ? <p>Loading...</p> : dienasJSX}
+
+    </>)
 }
 
 export default App;
